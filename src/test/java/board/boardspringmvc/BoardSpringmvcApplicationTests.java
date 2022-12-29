575323d948d5a7f593da2b3ca691a56eb83a5a7f
seongjin <nature9545@gmail.com>
2022-12-29 23:35:03 +0900

    게시판 프로젝트 Initial commit

---
+++ src/test/java/board/boardspringmvc/BoardSpringmvcApplicationTests.java
@@ -0,0 +1,13 @@
+package board.boardspringmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardSpringmvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
